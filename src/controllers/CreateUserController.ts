f7a19d57814a2b6ad6fa1252a2d1d761bcd8cd3f
Gabriel Resende <gabriel.arantesresende@gmail.com>
2021-09-07 15:08:25 -0300

    class 3 started, error handling working

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -3,16 +3,13 @@ import { CreateUserService } from "../services/CreateUserService";
 
 class CreateUserController {
      async handle(req: Request, res: Response) {
-          console.log("Req iniciada");
 
           const { name, email, admin } = req.body;
 
           const createUserService = new CreateUserService();
-          console.log("Usuario vai ser cadastrado");
 
           const user = await createUserService.execute({ name, email, admin });
 
-          console.log("Usuario cadastrado");
           return res.status(200).json(user);
 
      }
