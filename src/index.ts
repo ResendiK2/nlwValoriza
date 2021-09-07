c1a472c676ef0b51bcc42f6e474ffbc01e189b54
Gabriel Resende <gabriel.arantesresende@gmail.com>
2021-09-06 23:31:52 -0300

    second class, problem with typeorm

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.get("/test", (req, res) => {
-     return res.send("Olá NLW!")
-})
-
-app.listen(3000, () => console.log('Server is running'));
